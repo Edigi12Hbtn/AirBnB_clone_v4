210ba51316dc90bf3a4ff520397749775170e0d6
Steben Arias <1638@holbertonschool.com>
2020-09-28 19:28:44 -0500

    [Feature] Task 2 - Adding new javascript for the filter of amenities section (Dynamic)

--- web_dynamic/static/script/1-hbnb.js
+++ web_dynamic/static/script/1-hbnb.js
@@ -1,13 +1,16 @@
-let amenitiesIds = {};
+#!/usr/bin/node
+/*
+script that Listen for changes on each INPUT checkbox tag.
+*/
+const amenitiesIds = {};
 
-$(document).ready(function(){
-  $('input[type="checkbox"]').click(function(){
-    if($(this).is(":checked")){
-      amenitiesIds[$(this).attr("data-id")] = $(this).attr("name");
+$(document).ready(function () {
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      amenitiesIds[$(this).attr('data-id')] = $(this).attr('name');
+    } else {
+      delete amenitiesIds[$(this).attr('data-id')];
     }
-    else {
-      delete amenitiesIds[$(this).attr("data-id")];
-    }
-      $('.amenities h4').text(Object.values(amenitiesIds).join(', '));
+    $('.amenities h4').text(Object.values(amenitiesIds).join(', '));
   });
-});		
+});
